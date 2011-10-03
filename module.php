a73724a7fad6a87e7b6675d4ef63acc2a27c907b
mykiimike <mv@verman.fr>
2011-10-03 19:26:14 +0000

    Releasing 1.0.0
    git-svn-id: file:///home/svn/nat/opendoc/stable/1_0_0@9 1574276b-02a8-49be-a977-9cc44bf2a996

--- module.php
+++ module.php
@@ -7,8 +7,8 @@ class wfm_opendoc extends wf_module {
 	
 	public function get_name() { return("opendoc"); }
 	public function get_description()  { return("OWF Native OpenDocument manager"); }
-	public function get_banner()  { return("opendoc/1.0.0.RC2"); }
-	public function get_version() { return("1.0.0.RC2"); }
+	public function get_banner()  { return("opendoc/1.0.0"); }
+	public function get_version() { return("1.0.0"); }
 	public function get_authors() { return("Michael VERGOZ"); }
 	public function get_depends() { return(NULL); }
 	
